57cfd49387bfa8d7e6818468c81b4e2f30332179
Josh Lee <josh@joshuamlee.com>
2023-01-20 10:20:24 -0500

    config from petr

--- install-agent-via-helm.sh
+++ install-agent-via-helm.sh
@@ -4,10 +4,12 @@ helm install instana-agent \
    --repo https://agents.instana.io/helm \
    --namespace instana-agent \
    --create-namespace \
-   --set agent.endpointHost=ingress-red-saas.instana.io \
+   --set agent.key=$INSTANA_AGENT_KEY \
+   --set agent.downloadKey=$INSTANA_DOWNLOAD_KEY \
+   --set agent.endpointHost=$INSTANA_ENDPOINT_HOST \
    --set agent.endpointPort=443 \
-   --set cluster.name='otel-demo-w-agent' \
-   --set zone.name='narnia' \
+   --set cluster.name='otel-demo' \
+   --set zone.name='otel-demo' \
    --set service.create=true \
    --set opentelemetry.enabled=true \
    --values instana-agent-values.yaml \
